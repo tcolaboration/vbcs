040593b32cae09b035f8a7c408320189071ec070
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2020-02-08 02:55:20 +0200

    Add files via upload

---
+++ radu1.js
@@ -0,0 +1,9 @@
+/* This liberary is developed by radu.marinache@oracle.com -CX APPS TECH team 2020-02-07 */
+
+
+// function used to show console log messages
+function  logVBCS(value){
+    console.log(" ------> " + value);
+}
+
+//'oj-odcs/knowledge-common/KnowledgeCommon'
