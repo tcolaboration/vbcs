c8104c265f293fdc5557159f1f898377e7f5df07
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2020-02-08 03:05:45 +0200

    Add files via upload

---
+++ radu_1.js
@@ -0,0 +1,15 @@
+/* This liberary is developed by radu.marinache@oracle.com -CX APPS TECH team 2020-02-07 */
+
+
+// function used to show console log messages
+function  logVBCS(value){
+    console.log(" ------> " + value);
+}
+
+function getElementByID(ID){
+document.getElementById(ID)
+
+    
+}
+odcs-home-page-chat
+logVBCS(getElementByID("odcs-home-page-chat"));
