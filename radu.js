7b45a11213d2cd659e0f5db7f49cf2bf1cf97308
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2020-02-08 03:05:23 +0200

    Delete radu.js

--- radu.js
+++
@@ -1,8 +0,0 @@
-/* This liberary is developed by radu.marinache@oracle.com -CX APPS TECH team 2020-02-07 */
-
-
-// function used to show console log messages
-function  logVBCS(value){
-    console.log(" ------> " + value);
-}
-logVBCS("HELLO   WORLD");
