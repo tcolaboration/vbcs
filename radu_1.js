39065c0b6679948284c73e07ecd950a7a266725a
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2020-02-08 03:01:07 +0200

    Add files via upload

---
+++ radu_1.js
@@ -0,0 +1,8 @@
+/* This liberary is developed by radu.marinache@oracle.com -CX APPS TECH team 2020-02-07 */
+
+
+// function used to show console log messages
+function  logVBCS(value){
+    console.log(" ------> " + value);
+}
+logVBCS("HELLO   WORLD");
