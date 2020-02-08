7977997dc5db65d352c2447f62833fb462cc7a32
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2020-02-08 02:03:12 +0200

    console log function

---
+++ radu.js
@@ -0,0 +1,8 @@
+/* This liberary is developed by radu.marinache@oracle.com -CX APPS TECH team 2020-02-07 */
+
+
+// function used to show console log messages
+function  log(value){
+    console.log(value);
+}
+
